8ea460c197a611fe3587f4e2166db5d10300ebe1
JaagupPr <158137390+JaagupPr@users.noreply.github.com>
2025-06-02 01:16:42 +0300

    add host environment mock and macro interfaces

---
+++ Car/Car.CarTest/Mock/MockHostEnvironment.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace Car.CarTest.Mock
+{
+    public class MockIHostEnvironment : IHostEnvironment
+    {
+        public string EnvironmentName { get; set; } = "Test";
+        public string ApplicationName { get; set; } = "Car.Test";
+        public string ContentRootPath { get; set; } = Directory.GetCurrentDirectory();
+        public IFileProvider ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    }
